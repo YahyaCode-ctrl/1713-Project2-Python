4101540a525f9829f7286951be4fb3c6651cb02a
Yahya Elhadri <yahyaelhadri@iesmontsia.org>
2025-11-07 08:45:12 +0100

    Add files via upload

---
+++ VS code/Training 2 (Exercici1).py
@@ -0,0 +1,5 @@
+#Training: Exercici 1:
+print('Bon Dia!!')
+a= float(input('Introdueix la longitud del cuadrat: '))
+a= a*a
+print("làrea del cuadrat és: ", a ,"cm2")
---
+++ VS code/Training 3 (Exercici1).py
@@ -0,0 +1,8 @@
+#Training 3: Condicionals
+nom = input("Com et dius? ")
+any = int(input("Quants any tens? "))
+
+if any >= 18 :
+	print(nom ,"és major d'edat")
+elif any < 18 :
+	print(nom ,"és menor d'edat")
