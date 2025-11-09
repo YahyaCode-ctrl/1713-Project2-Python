0b202b10ea9e541353e977d2f9c6e9374b7d41bb
Yahya Elhadri <yahyaelhadri@iesmontsia.org>
2025-11-09 12:15:24 +0100

    Delete VS code directory

--- VS code/Training 2 (Exercici1).py
+++
@@ -1,5 +0,0 @@
-#Training: Exercici 1:
-print('Bon Dia!!')
-a= float(input('Introdueix la longitud del cuadrat: '))
-a= a*a
-print("làrea del cuadrat és: ", a ,"cm2")
--- VS code/Training 3 (Exercici1).py
+++
@@ -1,8 +0,0 @@
-#Training 3: Condicionals
-nom = input("Com et dius? ")
-any = int(input("Quants any tens? "))
-
-if any >= 18 :
-	print(nom ,"és major d'edat")
-elif any < 18 :
-	print(nom ,"és menor d'edat")
