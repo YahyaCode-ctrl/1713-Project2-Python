adad6ad9a71f8c72ca94cabb08dbfc420b274df1
YahyaCode-ctrl <yahyaelhadri@iesmontsia.org>
2025-11-13 16:35:54 +0100

    delete a file

--- Esborany.py
+++
@@ -1,6 +0,0 @@
-#En aquest arxiu, apprendrem a practicar noves coses en Python.
-age=int(input("How old are you: "))
-while age < 0:
-    print(f"an {age} cannot be negative")
-    age=int(input("How old are you: "))
-print(f"You are {age} old !!")
